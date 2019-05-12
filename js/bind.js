f428cbe1ea84af51681b52984b9a05214ee69636
马啸 <m543438924@163.com>
2019-05-12 23:30:55 +0800

    change bind

--- js/bind.js
+++ js/bind.js
@@ -1,20 +1,21 @@
-// Function.prototype.mybind=function(arg){
-//     if(typeof this!="function")return;
-//     var _this = this;
-//     var args = Array.prototype.slice.call(arguments,1);
-//     return function(){
-//         return _this.apply(arg,args.concat(Array.prototype.slice.call(arguments)))
-//     }
-// }
-
-Function.prototype.mybind=function(arg){
-    if(typeof this!=="function")return;
-    var _self =this;
-    var args = Array.prototype.slice.call(arguments,1);
-    var fnBound = function(){
-        var _this = this instanceof _self? this:arg;
-        return _self.apply(_this,args.concat(Array.prototype.slice.call(arguments)))
-    } 
-    fnBound.prototype = this.prototype;
-    return fnBound;
+Function.prototype.bind=function( _this){
+    if( typeof this !=="function"){
+        throw new Error("请使用函数调用bind");
+    }
+    var slice = Array.prototype.slice;
+    var _fBindThis = this;
+    var args = slice.call(arguments , 1 );
+    var fNop=function(){};
+    var fBound = function(){
+        var _args =args.concat(slice.call(arguments));
+        //this instanceof fBound ===true  说明返回fBound 被 new 了
+        var ifisNew = this instanceof fBound;
+        return _fBindThis.apply(ifisNew?this:_this,_args);
+    };
+    //维护自己的 原型链
+    if(this.prototype){
+        fNop.prototype=this.prototype;
+    }
+    fBound.prototype = new fNop();
+    return fBound ;
 }
